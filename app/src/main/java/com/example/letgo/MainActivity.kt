39c8a4210607e4f14e9b5e9eabbb0af1d52c9f92
WONG HORNG JUN <hj.wong02@gmail.com>
2023-05-24 00:54:55 +0800

    Added Material Design into project
    Proceed to Login Register View and custom widgets

--- app/src/main/java/com/example/letgo/MainActivity.kt
+++ app/src/main/java/com/example/letgo/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.letgo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import com.example.letgo.ui.theme.LetgoTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,24 +29,58 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello hi $name!")
+fun MainScreen() {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Home", "Settings", "Favorites")
+    val icons = listOf(
+        Icons.Filled.Home, Icons.Filled.Settings,
+        Icons.Filled.Favorite)
+
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        SmallTopAppBar(title = { Text("ThemeDemo", color = MaterialTheme.colorScheme.primary) }, scrollBehavior = null)
+
+        Button(onClick = { },
+        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.tertiary)) {
+            Text("MD3 Button", color = MaterialTheme.colorScheme.background)
+        }
+
+        Text("A Theme Demo")
+
+        FloatingActionButton(onClick = { }) {
+            Text("FAB")
+        }
+
+        NavigationBar {
+            items.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    icon = { Icon(icons[index], contentDescription = null) },
+                    label = { Text(item) },
+                    selected = selectedItem == index,
+                    onClick = { selectedItem = index }
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     LetgoTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }

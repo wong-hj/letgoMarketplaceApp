96f8080f42f03bc3d6204f0f29cca3998853fc04
WONG HORNG JUN <hj.wong02@gmail.com>
2023-05-26 03:54:39 +0800

    Update Liked.kt

--- app/src/main/java/com/example/letgo/screens/Liked.kt
+++ app/src/main/java/com/example/letgo/screens/Liked.kt
@@ -3,11 +3,13 @@ package com.example.letgo.screens
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.*
-import androidx.compose.material.Scaffold
 import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -21,11 +23,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
+import com.example.letgo.R
 import com.example.letgo.models.Products
+import com.example.letgo.nav.Routes
 import com.example.letgo.ui.theme.Typography
-import com.example.letgo.viewModel.LikedViewModel
 import com.example.letgo.widgets.CustomBottomBar
 import com.example.letgo.widgets.CustomHeader
+import com.example.letgo.viewModel.LikedViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -37,54 +41,55 @@ fun Liked(navController: NavHostController, likeVM: LikedViewModel = viewModel()
     Scaffold(
 
         content = {
-            CustomHeader(value = "Liked.")
+            Column(modifier = Modifier.fillMaxWidth()) {
+                CustomHeader(value = "Liked.")
 
-            Spacer(modifier = Modifier.height(20.dp))
+                Spacer(modifier = Modifier.height(20.dp))
 
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
-                verticalArrangement = Arrangement.spacedBy(12.dp),
-                horizontalArrangement = Arrangement.spacedBy(12.dp),
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(start = 20.dp, end = 20.dp, bottom = 70.dp),
-            ) {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                    modifier = Modifier
+                        .padding(start = 20.dp, end = 20.dp, bottom = 70.dp),
+                ) {
 
-                items(products) { product ->
-                    Card(
-                        onClick = { /* Do something */ },
-                        modifier = Modifier
-                            .size(width = 180.dp, height = 200.dp)
-                    ) {
-
-                        AsyncImage(
-                            model = product.imageURL,
-                            contentDescription = null,
-                            contentScale = ContentScale.FillWidth,
+                    items(products) { product ->
+                        Card(
+                            onClick = { /* Do something */ },
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .height(140.dp)
-                        )
-
-                        val text = buildAnnotatedString {
-                            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-                                append(product.name)
-                            }
-                            append("\nRM ${product.price} | ${product.quality}")
-                        }
-
-                        Box(
-                            modifier = Modifier
-                                .fillMaxSize(),
-                            contentAlignment = Alignment.Center
+                                .size(width = 180.dp, height = 200.dp)
                         ) {
-                            Text(
-                                text = text,
-                                modifier = Modifier.align(Alignment.Center),
-                                textAlign = TextAlign.Center,
-                                style = Typography.body2
 
+                            AsyncImage(
+                                model = product.imageURL,
+                                contentDescription = null,
+                                contentScale = ContentScale.FillWidth,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(140.dp)
                             )
+
+                            val text = buildAnnotatedString {
+                                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                    append(product.name)
+                                }
+                                append("\nRM ${product.price} | ${product.quality}")
+                            }
+
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(
+                                    text = text,
+                                    modifier = Modifier.align(Alignment.Center),
+                                    textAlign = TextAlign.Center,
+                                    style = Typography.body2
+
+                                )
+                            }
                         }
                     }
                 }

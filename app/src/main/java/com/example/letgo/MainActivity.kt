e9c348f4ee8fdb4b632bf7ebc4b53dc6e32b5c55
june <hj.wong02@gmail.com>
2023-06-10 14:21:28 +0800

    Implement Splash Screen

--- app/src/main/java/com/example/letgo/MainActivity.kt
+++ app/src/main/java/com/example/letgo/MainActivity.kt
@@ -10,12 +10,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.letgo.ui.theme.LetgoTheme
 import com.example.letgo.screens.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        Thread.sleep(1000)
+        installSplashScreen()
         setContent {
             LetgoTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,7 +27,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
                     MainScreen()
                 }
             }
